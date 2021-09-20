96158fa841b57053764213245004f18f0061dce8
MunozChristian110 <32554092+MunozChristian110@users.noreply.github.com>
2021-09-20 15:28:16 -0700

    Set up user creation forms, login, and logout

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, "Your account has been successfully created! You are now able to log in!")
             #password = form.cleaned_data.get('password')
-            return redirect('blog-home')
+            return redirect('login')
         else:
             messages.warning(request, "Failed to create account! Something went wrong!")
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
